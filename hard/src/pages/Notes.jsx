a471664368b28e5b6beed3482b76cb1d9bd5769e
David <soydoft@gmail.com>
2024-07-22 18:56:44 -0400

    Added remove functionality

--- hard/src/pages/Notes.jsx
+++ hard/src/pages/Notes.jsx
@@ -2,25 +2,30 @@ import { useState } from 'react'
 import styles from './Notes.module.css'
 
 export default function Notes() {
-    const [ notes, setNotes ] = useState([{title: 'title', description: 'desc...'}]);
+    const [ notes, setNotes ] = useState([]);
+    function addNote(e) {
+        e.preventDefault();
+        setNotes(() => [...notes, {id: notes.length, title: e.target[0].value, description: e.target[1].value}])
+    }
 
     function removeNotes(e) {
-        console.log(e);
-        console.log('removed');
+        e.preventDefault();
+        setNotes(() => notes.filter((note) => note.id != e.target.parentElement.id));
     }
 
     return (
         <>
-         <form>
+         <form onSubmit={(e) => addNote(e)}>
             <input type="text" placeholder="Note Title"/>
             <input type="text" placeholder="Note Description"/>
             <input type="submit" value="Add"/>
         </form>
-        {notes?.map(note => {
+        {notes?.map((note) => {
             return (
-                <div className={styles.note} key={notes.indexOf(note)}>
+                <div className={styles.note} key={note?.id} id={note?.id}>
                 <p>{note?.title}</p>
                 <p>{note?.description}</p>
+                <p style={{display:"none"}}></p>
                 <button onClick={(e) => removeNotes(e)}>Remove</button>
                 </div>
             )
