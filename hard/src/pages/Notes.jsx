0d45cae6d6ee015b74809d8873e4c7297ad58508
David <soydoft@gmail.com>
2024-07-22 21:30:57 -0400

    Added add note functionality, remove note functionality, and edit note functionality

--- hard/src/pages/Notes.jsx
+++ hard/src/pages/Notes.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Notes.module.css'
 
+let targetNote;
 export default function Notes() {
     const [ notes, setNotes ] = useState([]);
     function addNote(e) {
@@ -11,6 +13,27 @@ export default function Notes() {
     function removeNotes(e) {
         e.preventDefault();
         setNotes(() => notes.filter((note) => note.id != e.target.parentElement.id));
+    }
+
+    function openEditor(e) {
+        e.preventDefault();
+        console.log(e.target.parentElement);
+        e.target.parentElement.nextSibling.style.display = 'block';
+        targetNote = e.target.parentElement;
+    }
+
+    function editNotes(e) {
+        e.preventDefault();
+
+        const index = notes.findIndex((note) => note.id == targetNote.id);
+        console.log(index);
+        setNotes((notes) => {
+            const newNotes = [...notes];
+            newNotes[index] = {id: Number(targetNote.id, 10), title: e.target[0].value, description: e.target[1].value}
+            return newNotes;
+        });
+        e.target.style.display = 'none';
+        console.log(notes);
     }
 
     return (
@@ -27,9 +50,17 @@ export default function Notes() {
                 <p>{note?.description}</p>
                 <p style={{display:"none"}}></p>
                 <button onClick={(e) => removeNotes(e)}>Remove</button>
+                <button onClick={(e) => openEditor(e)}>Edit</button>
+                <Link to="/details" >Details</Link>
+
                 </div>
             )
         })}
+        <form className={styles.editor} onSubmit={(e) => editNotes(e)}>
+            <input type="text" placeholder="Note Title"/>
+            <input type="text" placeholder="Note Description"/>
+            <input type="submit" value="Finish Edit"/>
+        </form>
         </>
     )
 }
